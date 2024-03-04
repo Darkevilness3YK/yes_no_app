4582c709b6b9bfad9fe7f0ff532c47da4b8fbd7f
Alan Ordoñez <Darkevilness3YK@gmail.com>
2024-03-04 00:13:15 -0600

    Message Input form field settings added.

--- lib/widgets/shared/message_field_box.dart
+++ lib/widgets/shared/message_field_box.dart
@@ -5,18 +5,24 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(40.0),
     );
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         onPressed: () {
-          print('Message box message.');
+          final userMessage = textController.value.text;
+          print(userMessage);
+          textController.clear();
         },
         icon: const Icon(Icons.send_outlined),
       ),
@@ -24,11 +30,15 @@ class MessageFieldBox extends StatelessWidget {
 
     return TextFormField(
       decoration: inputDecoration,
-      onChanged: (value) {
-        print('Current message: $value');
+      controller: textController,
+      focusNode: focusNode,
+      onTapOutside: (event) {
+        focusNode.unfocus();
       },
       onFieldSubmitted: (value) {
         print('Message submitted: $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
