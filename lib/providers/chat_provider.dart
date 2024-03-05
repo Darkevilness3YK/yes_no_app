ce417c8642947f575d82d8e9c93942cc7fc154c5
Alan Ordoñez <Darkevilness3YK@gmail.com>
2024-03-04 20:40:09 -0600

    Changed List<Message> name from message to messageList

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/models/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: '¡Hola!', fromWho: FromWho.me),
     Message(text: '¿Me das dinero?', fromWho: FromWho.me),
   ];
