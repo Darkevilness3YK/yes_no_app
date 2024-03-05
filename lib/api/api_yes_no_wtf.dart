8c02c4b23201090d9fa80ff14dbddcb1d5027bbd
Alan Ordoñez <Darkevilness3YK@gmail.com>
2024-03-04 22:56:39 -0600

    YesNoModel updated using QuickType to auto generate a model using the JSON response of the API used in this app. Also, created toMessageEntity in YesNoModel to return a message based on the API response.

--- lib/api/api_yes_no_wtf.dart
+++ lib/api/api_yes_no_wtf.dart
@@ -1,18 +1,65 @@
-import 'package:dio/dio.dart';
+//! Without using QuickType extension.
+// import 'package:dio/dio.dart';
+// import 'package:yes_no_app/models/message.dart';
+// import 'package:yes_no_app/models/yes_no_model.dart';
+
+// class ApiYesNoWtf {
+//   final _dio = Dio();
+
+//   Future<Message> GetYesNoAnswer() async {
+//     final response = await _dio.get('https://yesno.wtf/api');
+//     final yesNoModel = YesNoModel.fromJson(response.data);
+
+//     return Message(
+//       text: yesNoModel.answer,
+//       fromWho: FromWho.friend,
+//       imageUrl: yesNoModel.image,
+//     );
+//   }
+// }
+
+//! Using QuickType extension (or going to https://app.quicktype.io/)
+
+// To parse this JSON data, do
+//
+//     final yesNoModel = yesNoModelFromJson(jsonString);
+
+import 'dart:convert';
+
 import 'package:yes_no_app/models/message.dart';
-import 'package:yes_no_app/models/yes_no_model.dart';
 
-class ApiYesNoWtf {
-  final _dio = Dio();
+YesNoModel yesNoModelFromJson(String str) => YesNoModel.fromJson(json.decode(str));
 
-  Future<Message> GetYesNoAnswer() async {
-    final response = await _dio.get('https://yesno.wtf/api');
-    final yesNoModel = YesNoModel.fromJson(response.data);
+String yesNoModelToJson(YesNoModel data) => json.encode(data.toJson());
 
+class YesNoModel {
+  final String answer;
+  final bool forced;
+  final String image;
+
+  YesNoModel({
+    required this.answer,
+    required this.forced,
+    required this.image,
+  });
+
+  factory YesNoModel.fromJson(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() {
     return Message(
-      text: yesNoModel.answer,
+      text: answer == 'yes' ? 'Sí' : 'No',
       fromWho: FromWho.friend,
-      imageUrl: yesNoModel.image,
+      imageUrl: image,
     );
   }
 }
