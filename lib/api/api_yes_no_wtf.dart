af298ba2436b5be22bcece49da3a7c774c2fecd5
Alan Ordoñez <Darkevilness3YK@gmail.com>
2024-03-04 23:06:47 -0600

    ApiYesNoWtf updated to return an instance of Message class

--- lib/api/api_yes_no_wtf.dart
+++ lib/api/api_yes_no_wtf.dart
@@ -5,14 +5,10 @@ import 'package:yes_no_app/models/yes_no_model.dart';
 class ApiYesNoWtf {
   final _dio = Dio();
 
-  Future<Message> GetYesNoAnswer() async {
+  Future<Message> getYesNoAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
     final yesNoModel = YesNoModel.fromJson(response.data);
 
-    return Message(
-      text: yesNoModel.answer,
-      fromWho: FromWho.friend,
-      imageUrl: yesNoModel.image,
-    );
+    return yesNoModel.toMessageEntity();
   }
 }
